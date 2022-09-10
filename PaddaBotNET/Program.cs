39fb9a320f62e4b17475a431a9f6224d467b5d8e
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-10 22:44:37 +0200

    Pretty

--- PaddaBotNET/Program.cs
+++ PaddaBotNET/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PaddaBotNET {
-    internal class Program {
-        static void Main(string[] args) {
+namespace PaddaBotNET 
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+        {
         }
     }
 }
