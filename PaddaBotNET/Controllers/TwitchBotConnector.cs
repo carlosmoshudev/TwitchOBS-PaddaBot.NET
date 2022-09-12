b4bf4d3b7abf5b4c306f0a858d4f7676d646cb45
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-12 09:14:03 +0200

    Config Data files

--- PaddaBotNET/Controllers/TwitchBotConnector.cs
+++ PaddaBotNET/Controllers/TwitchBotConnector.cs
@@ -1,6 +1,10 @@
-﻿namespace PaddaBotNET.Controllers 
-{
-    internal class TwitchBotConnector 
-    {
+﻿namespace PaddaBotNET.Controllers {
+    internal class TwitchBotConnector {
+        public string bot, oauth, channel;
+        public TwitchBotConnector(string bot, string oauth, string channel) {
+            this.bot = bot;
+            this.oauth = oauth;
+            this.channel = channel;
+        }
     }
 }
