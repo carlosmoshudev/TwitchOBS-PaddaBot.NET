84ff68876c51a19158ed2c1406f9f01145c74e6c
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-13 21:44:01 +0200

    ChatBot Model

---
+++ PaddaBotNET/Models/ChatBots.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace PaddaBotNET.Models {
+    class ChatBots {
+        TwitchClient client;
+        public ChatBots(Credentials credentials) {
+            ConnectionCredentials botCredentials = new ConnectionCredentials
+            (
+                twitchUsername: credentials.account, 
+                twitchOAuth: credentials.token
+            );
+            ClientOptions options = new ClientOptions {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+        }
+    }
+}
