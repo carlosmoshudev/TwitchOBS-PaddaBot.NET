1c1a496bd409308ecf6c794e74321d7d7454eb91
carlosmoshudev <carlos@carlosmoshu.com>
2022-09-14 21:22:10 +0200

    Add Event Handler

---
+++ PaddaBotNET/Events/BotAccountEvents.cs
@@ -0,0 +1,33 @@
+﻿using PaddaBotNET.Interfaces;
+
+namespace PaddaBotNET.Events {
+    internal class BotAccountEvents : IBotEventHandler {
+        public void ChannelPointsRedeemedHandler() {
+            throw new System.NotImplementedException();
+        }
+
+        public void ConnectedHandler() {
+            throw new System.NotImplementedException();
+        }
+
+        public void GotRaidedHandler() {
+            throw new System.NotImplementedException();
+        }
+
+        public void MessageReceivedHandler() {
+            throw new System.NotImplementedException();
+        }
+
+        public void NewSubscriberHandler() {
+            throw new System.NotImplementedException();
+        }
+
+        public void PubSubConnected() {
+            throw new System.NotImplementedException();
+        }
+
+        public void UserJoinedHandler() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PaddaBotNET/Interfaces/IBotEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace PaddaBotNET.Interfaces {
+    interface IBotEventHandler {
+        void MessageReceivedHandler();
+        void NewSubscriberHandler();
+        void GotRaidedHandler();
+        void UserJoinedHandler();
+        void ConnectedHandler();
+        void ChannelPointsRedeemedHandler();
+        void PubSubConnected();
+    }
+}
